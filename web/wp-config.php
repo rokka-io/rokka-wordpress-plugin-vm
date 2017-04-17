fc61de7a81e507f43162bb0e7958c775ff736cfe
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-04-17 17:32:29 +0200

    remove unused filter

--- web/wp-config.php
+++ web/wp-config.php
@@ -94,6 +94,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
-
-// Disable XML RPC
-add_filter( 'xmlrpc_enabled', '__return_false' );
