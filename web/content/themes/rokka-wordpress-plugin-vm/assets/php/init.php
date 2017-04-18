dc68525a886ed1a647b589ec4328268d6704ee02
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-04-18 21:47:09 +0200

    define image sizes

--- web/content/themes/rokka-wordpress-plugin-vm/assets/php/init.php
+++ web/content/themes/rokka-wordpress-plugin-vm/assets/php/init.php
@@ -38,6 +38,37 @@ function theme_setup() {
 	// =======================================================================//
 
 	add_theme_support( 'title-tag' );
+
+	rokka_define_image_sizes();
 }
 
 add_action( 'after_setup_theme', 'theme_setup' );
+
+/**
+ * Define image sizes to test rokka stacks
+ */
+function rokka_define_image_sizes() {
+	// Image size medium 375w <unlimited>h
+	update_option( 'medium_size_w', 370 );
+	update_option( 'medium_size_h', 9999 );
+
+	// Image size medium_large 750w <unlimited>h
+	update_option( 'medium_large_size_w', 735 );
+	update_option( 'medium_large_size_h', 9999 );
+
+	// Image size medium-large-to-large 1250w <unlimited>h
+	add_image_size( 'medium-large-to-large', 1250, 9999 );
+
+	// Image size responsive can be used to stretch image to the full with of the container.
+	add_image_size( 'responsive', 1250, 9999 );
+
+	// Image size large 1500w <unlimited>h
+	update_option( 'large_size_w', 1500 );
+	update_option( 'large_size_h', 9999 );
+
+	// Image size colmd4-image is used render different sizes attribute for images in a col-md-4 column.
+	add_image_size( 'colmd4-image', 1500, 9999 );
+
+	// Image size teaser 750w 375h cropped
+	add_image_size( 'teaser', 750, 375, true );
+}
